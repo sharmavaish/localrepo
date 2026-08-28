52fb3f4208de540ff0b967c97871f75ab7654e31
sharmavaish <8.vaishali@gmail.com>
2026-08-28 16:28:57 -0230

    New test unit cases

---
+++ tests/test_operations.py
@@ -0,0 +1,12 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+def test_sub():
+    assert sub(3,5)==-2
+    assert sub(8,7)==1
+    assert sub(10,5)==5
+    assert sub(4,5)==-1
+    
